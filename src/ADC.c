4c00a9383750b77ce20ee4b50cd6e6952bd64b17
josuesorbera <josuesorbera1@hotmail.com>
2026-06-15 03:02:16 -0300

    fixing bugs and aplicated clang format

--- src/ADC.c
+++ src/ADC.c
@@ -2,23 +2,20 @@
 #include "ADC.h"
 
 void ADC_Config(void) {
-    ADC_Init(200000); // ADC clock is 200KHz
-    ADC_PinConfig(ADC_CHANNEL_0); // Configure pin for channel 0
-    ADC_ChannelEnable(ADC_CHANNEL_0); // Enable channel 0
+    ADC_Init(200000);                   // ADC clock is 200KHz
+    ADC_PinConfig(ADC_CHANNEL_0);       // Configure pin for channel 0
+    ADC_ChannelEnable(ADC_CHANNEL_0);   // Enable channel 0
     ADC_StartCmd(ADC_START_CONTINUOUS); // Start continuous conversion
-    ADC_BurstEnable(); // Enable burst mode
-    ADC_PowerDown(); // Iniciates turned off so we can can start it with a button
+    ADC_BurstEnable();                  // Enable burst mode
+    ADC_PowerDown();                    // Iniciates turned off so we can can start it with a button
 }
 
-void get_ADC_Value(void)
-{
+void get_ADC_Value(void) {
     uint16_t adcValue;
 
-    if(ADC_ChannelGetStatus(ADC_CHANNEL_0, ADC_DATA_DONE))
-    {
+    if (ADC_ChannelGetStatus(ADC_CHANNEL_0, ADC_DATA_DONE)) {
         adcValue = ADC_ChannelGetData(ADC_CHANNEL_0);
 
         servo_Position(adcValue);
     }
 }
-
